284d936854567fe8ef8bdc53612919d8e70484d8
LiuChao <Josh@LiuChaodeMacBook-Pro.local>
2015-03-18 17:34:11 +0800

    Add Register view controller

---
+++ iWantTo/iWantTo/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  iWantTo
+//
+//  Created by LiuChao on 3/18/15.
+//  Copyright (c) 2015 Josh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
