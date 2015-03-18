da9b64a0b8c07cf7adccd7a03a25e488ff287d6f
iwolffy <josh.liu@icloud.com>
2015-03-18 15:37:40 +0800

    Initial commit

---
+++ iWantTo-iOS/iWantTo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iWantTo
+//
+//  Created by LiuChao on 3/18/15.
+//  Copyright (c) 2015 LiuChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
